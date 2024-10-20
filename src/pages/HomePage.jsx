6f4669ffb15eb09b94eb6b0ae980626381ddbd16
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-10-20 16:12:24 +0530

    Second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -76,7 +76,7 @@ const HomePage = () => {
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div
           className="relative z-10 flex flex-col items-flex-start justify-center h-full text-white"
-          style={{ marginLeft: "150px" }} 
+         
         >
           <h2 className="text-3xl font-bold">Get 15% off your rental</h2>
           <p className="mt-4 text-lg">
